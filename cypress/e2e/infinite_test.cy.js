f4693cae8691a08b2cc62b98158753ed5a7fef7f
Sunita Kunwar <sunitakunwar@Sunitas-MacBook-Air.local>
2026-03-28 22:24:58 +0545

    Initial commit

---
+++ cypress/e2e/infinite_test.cy.js
@@ -0,0 +1,29 @@
+describe('Contact Form Test', () => {
+
+    it('Fill form correctly', () => {
+  
+      cy.visit('https://www.infinite.com/');
+  
+      cy.contains('Contact').click();
+      cy.url().should('include', '/contact/');
+  
+      // Work only inside visible form
+      cy.get('form:visible').within(() => {
+  
+        cy.get('input[placeholder*="Full Name"]:visible')
+          .type('Sunita Kunwar')
+          .should('have.value', 'Sunita Kunwar');
+  
+        cy.get('input[placeholder*="Title"]:visible')
+          .type('QA Engineer')
+          .should('have.value', 'QA Engineer');
+  
+        cy.get('input[type="email"]:visible')
+          .type('sunita.kunwar@test.com')
+          .should('have.value', 'sunita.kunwar@test.com');
+  
+      });
+  
+    });
+  
+  });
---
+++ cypress/e2e/practise_1.cy.js
@@ -0,0 +1,7 @@
+describe ('login page test', () =>{
+    it ('fill information', () =>{
+        cy.visit('https://testautomationpractice.blogspot.com/');
+        cy.get('#name').type('Sunita Kunwar');
+        cy.get('#textarea').type('write somethings');
+    });
+});
